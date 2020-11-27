64fdd42a07a428c8a2fc581edb61d178180493db
dangnam739 <kieudangnam99@gmail.com>
2020-11-27 01:59:58 +0000

    Add detail book

--- thefirstrails/db/schema.rb
+++ thefirstrails/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_20_165020) do
+ActiveRecord::Schema.define(version: 2020_11_27_015705) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
